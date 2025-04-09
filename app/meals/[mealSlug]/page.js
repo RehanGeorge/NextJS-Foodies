46aaa7fa7df39b0aa4b64c6d331822a205a07401
Rehan George <rehan.george@gmail.com>
2025-04-09 23:20:51 +0530

    Create a meals grid

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,4 +1,5 @@
-export default function MealDetailsPage({ params }) {
+export default async function MealDetailsPage({ params }) {
+  
   return (
     <div>
       <h1>This is the meal page for {params.mealSlug}</h1>
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
         <>
             <header className={classes.header}>
@@ -15,7 +18,7 @@ export default function MealsPage() {
                 <p className={classes.cta}><Link href="/meals/share">Share Your Favorite Recipe</Link></p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     )
