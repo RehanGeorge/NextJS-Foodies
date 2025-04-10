f2ab077161c642d80740125a2053482e728cc6b1
Rehan George <rehan.george@gmail.com>
2025-04-10 23:12:09 +0530

    Add meal page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,38 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
 export default async function MealDetailsPage({ params }) {
-  
+  const data = await params;
+  const meal = getMeal(data.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <div>
-      <h1>This is the meal page for {params.mealSlug}</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>
+      </main>
+    </>
   );
 }
--- app/meals/error.js
+++ app/meals/error.js
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <main className="error">
             <h1>An error ocurred!</h1>
-            <p>Failed to meet meal data. Please try again later.</p>
+            <p>Failed to get meal data. Please try again later.</p>
         </main>
     )
 }
